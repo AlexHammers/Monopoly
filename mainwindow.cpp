cba2aeab42b26cf9300f928df613b4a82def1ba2
Callie Johnston <calliej7@vt.edu>
2012-04-03 22:57:05 -0400

    Added some description of the button actions and other notes about the GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,30 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_diceButton_clicked()
+{
+    int diceRoll = (rand() & 12) + 1;
+
+    /*player.currentPlace += diceRoll;
+    if (player.currentPlace > 36)
+    {
+        player.currentPlace -= 36;
+    }
+
+    //move player picture (need coordinates!)
+    //display Place info and options
+    //if space has no options, move on to next player
+    */
+}
+
+void MainWindow::on_actionButton_clicked()
+{
+    //do action
+    //go to next player
+}
+
+//menu:
+//new game - single/multi/how many players
+//save game
+//resume game
