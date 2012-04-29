9bc99102572e03526b1effd3568e8dd05a8bc702
Alex Hammers <ahamm92@vt.edu>
2012-04-28 21:23:29 -0400

    Added funtionality to most space funtions. Changed some function parameters to better fit the program. Fixed some typos.

--- Monopoly.h
+++ Monopoly.h
@@ -7,41 +7,52 @@
 #include <string>
 
 // Enums
-enum SpaceType{_Property, _Railroad, _Utility,
-                _IncomeTax, _LuxuryTax, _CommunityChest,
-               _Chance, _Go, _Jail, _FreeParking, _GoToJail};
+enum SpaceType
+{
+	_Property,
+	_Railroad,
+	_Utility,
+	_IncomeTax,
+	_LuxuryTax,
+	_CommunityChest,
+	_Chance,
+	_Go,
+	_Jail,
+	_FreeParking,
+	_GoToJail
+};
 // End enums
 
 // Structs
 struct Player
 {
-int money;
-int curPos;
-int numOfUtilities;
-int numOfRailroads;
-bool inJail;
-int inJailCounter;
+	int money;
+	int curPos;
+	int numOfUtilities;
+	int numOfRailroads;
+	bool inJail;
+	int inJailCounter;
 };
 
 struct Space
 {
-  std::string name;
-  SpaceType spaceType;
-  int cost;
-  int rent[6];
-  int costPerHouse;
-  int groupedWith1;
-  int groupedWith2;
-  int numOfHouses;
-  int owner;
-  int xCoord;
-  int yCoord;
+	std::string name;
+	SpaceType spaceType;
+	int cost;
+	int rent[6];
+	int costPerHouse;
+	int groupedWith1;
+	int groupedWith2;
+	int numOfHouses;
+	int owner;
+	int xCoord;
+	int yCoord;
 };
 
 struct Cards
 {
-const std::string name;
-const int payOut; //Can be + or -
+	const std::string name;
+	const int payOut; //Can be + or -
 };
 // End Structs
 
@@ -49,21 +60,21 @@ const int payOut; //Can be + or -
 extern int numOfPlayers;
 extern int curPlayer;
 extern Space board[MaxBoardSize];
-extern Player players [MaxNumOfPlayers];
+extern Player players[MaxNumOfPlayers];
 // End Variables
 
 // Functions
 void initGame(int numPlayers);
 void Move(int numOfSpaces);
 void PayRent(int rent);
-void Property(int curPos);
-void Railroad(int numOfRailroadsOwned);
-void Utility(int numOfUtilitiesOwned);
+void Property();
+void Railroad();
+void Utility();
 void IncomeTax();
 void LuxuryTax();
 void CommunityChest();
 void Chance();
-void Go();
+//void Go();
 void GoToJail();
 bool evaluateJail(int dice1, int dice2);
 // End Functions
