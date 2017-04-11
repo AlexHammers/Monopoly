5b45c6e54e00c2cb05475be536dd271fa71ed0ab
Qingnan Duan <duanqn_own_1@yeah.net>
2017-04-11 16:48:24 +0800

    Changes: 1. Adapt to Qt 5.8.0 2. Add support for high DPI 3. Slightly change the user interface

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <QtGui/QApplication>
+#include <QApplication>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
